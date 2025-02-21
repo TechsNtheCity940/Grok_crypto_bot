a14589bba03ddb267bc90d523df43d28e73fd0e7
Mrogan <techsinthecity940@protonmail.com>
2025-02-21 03:49:06 -0600

    plz

--- strategies/momentum_strategy.py
+++ strategies/momentum_strategy.py
@@ -3,6 +3,10 @@ from stable_baselines3 import PPO
 class MomentumStrategy:
     def __init__(self, model_path='models/trained_models/ppo_trading_model'):  # Kept as is assuming absolute path
         self.model = PPO.load(model_path)
+        
+    def add_indicators(df):
+        df['rsi'] = compute_rsi(df['close'], window=14)
+        return df
 
     def get_action(self, observation):
         action, _ = self.model.predict(observation)
